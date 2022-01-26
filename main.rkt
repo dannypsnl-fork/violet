4b5487d35843de6cc92b5a88f383b5499927a9d6
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-01-26 21:08:16 +0800

    cmd setup

--- main.rkt
+++ main.rkt
@@ -1,1 +1,22 @@
 #lang racket/base
+(require racket/match
+         racket/function)
+
+(module+ main
+  (require racket/cmdline)
+
+  (command-line
+   #:program "violet"
+   #:args args
+   (cli args)))
+
+(define (cli args)
+  (match args
+    [(list "--help") (help)]
+    [(list "--version") (version)]
+    [_ (help)]))
+
+(define help
+  (thunk (displayln "violet -- a command-line tool for Racket")))
+(define version
+  (thunk (displayln "v0.0.1")))
