16c20dc44be1de57468fa7fdae91592a5dab8188
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-08 15:58:54 +0800

    init global env

--- main.rkt
+++ main.rkt
@@ -1,7 +1,8 @@
 #lang racket/base
 (require racket/match
          racket/function
-         racket/system)
+         racket/system
+         racket/file)
 
 (module+ main
   (require racket/cmdline)
@@ -11,11 +12,24 @@
    #:args args
    (cli args)))
 
+(define config-dir (build-path (find-system-path 'home-dir)
+                               ".violet"))
+(define bin-dir (build-path config-dir "bin"))
+(define src-dir (build-path config-dir "src"))
+(define lib-dir (build-path config-dir "lib"))
+
+(define (init-global)
+  (make-directory* bin-dir)
+  (make-directory* lib-dir)
+  (make-directory* src-dir))
+
 (define (cli args)
+  (init-global)
   (match args
     [(list "--help") (help)]
     [(list "--version") (version)]
     [(list "run")
+     (println (read (open-input-file "violet.ss")))
      (system* (find-executable-path "scheme") "--script" "main.ss")]
     [_ (help)]))
 
